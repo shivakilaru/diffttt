0d19ca77da94b4dd451d33f63d058a4e7b7cb767
Shiva Kilaru <shivakilaru@ShivaKilaru.local>
2013-05-06 13:47:28 -0400

    first commit

---
+++ app/controllers/eat_controller.rb
@@ -0,0 +1,7 @@
+class EatController < ApplicationController 
+	def food 
+		Resque.enqueue(Eat, params[:food])
+		render :text => "Put #{params[:food]} in fridge to eat later."
+	end
+ 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diffttt::Application.config.secret_token = 'a8717141155e90abfa4ce9615ee7f248bb723e2d40add7f0b233a157d3ea761234fd1e4ce4ed589c942c14775421aaad3c53460cdaf33d124294e6f1a40ae407'
